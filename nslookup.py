247a6848b421cdd10da4fdf6522a54e6688dc08e
Mathana Nair Sreedaran <mathana96@gmail.com>
2017-02-13 12:14:14 +0000

    Added nslookup. Need to clean output

---
+++ nslookup.py
@@ -0,0 +1,42 @@
+"""
+http://stackoverflow.com/questions/12297500/python-module-for-nslookup
+"""
+
+import subprocess
+import urllib
+import json
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    process = subprocess.Popen(["nslookup", "www.google.com"], stdout=subprocess.PIPE)
+    output = process.communicate()[0].split('\n')
+
+    IPlist = []
+    for data in output:
+        if 'Address' in data:
+            IPlist.append(data.replace('Address: ',''))
+    IPlist.pop(0)
+
+    print IPlist
+    
+
+    geolist = []
+    for ip in IPlist:
+        url = "http://ipinfo.io/" + ip + "/json"
+        print(url)
+        info = str(urllib.urlopen(url).read()) 
+        data = json.loads(info) 
+        if (("bogon" in data) == False):
+            # for geo in geolist:
+                # if (data['loc']) in geo.values():
+                #     geolist.remove(geo)
+
+            geolist.append(data)
+    print(geolist)
+
+    return render_template('template2.html', geolist=geolist)
+
+if __name__ == '__main__':
+    app.run(debug=True)
